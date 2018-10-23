dd2c876823d4984bdcd229cdabb4b200d7036a4d
Aaron Janke <janke.aaron@gmail.com>
2018-10-23 00:44:48 -0400

    optimized image

--- app/index.js
+++ app/index.js
@@ -1,47 +1,35 @@
-/**
- * Application entry point
- */
-
-// Load application styles
 import "styles/index.scss";
-
-// ================================
-// START YOUR APP HERE
-// ================================
-
 import * as GyroNorm from "../node_modules/gyronorm/dist/gyronorm.complete";
 
 document.addEventListener("DOMContentLoaded", () => {
     //CANVAS
-    let elementWidth = 320,
-        canvas = document.getElementById("canvas"),
+    let canvas = document.getElementById("canvas"),
         ctx = canvas.getContext("2d"),
         imageObj = new Image(),
-        height = 50,
-        width = 150,
+        imageHeight = 170,
+        imageWidth = 170,
         x = 10,
         y = 10;
 
     imageObj.src = "./assets/images/egg.png";
-    ctx.imageSmoothingEnabled = true;
 
     //ANIMATION
     let animate = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.width);
-        ctx.drawImage(imageObj, x, y, width, height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(imageObj, x, y, imageWidth, imageHeight);
     };
 
     //GYROSCOPE
     const gn = new GyroNorm();
 
-    imageObj.onload = () => {
+    imageObj.addEventListener("load", () => {
         gn.init()
             .then(() => {
                 gn.start(data => {
                     let currentX = data.dm.gx,
                         currentY = data.dm.gy;
 
-                    x + currentX < 0 || x + currentX > elementWidth / 2
+                    x + currentX < 0 || x + currentX > 160
                         ? null
                         : (x = x + currentX);
 
@@ -57,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .catch(e => {
                 // for devices without gyroscope
                 console.log(e);
-                ctx.drawImage(imageObj, 10, 10, width, height);
+                ctx.drawImage(imageObj, 10, 10, imageWidth, imageHeight);
             });
-    };
+    });
 });
